914ce0a6a373bcf3d2feb6bb55105223dd56193a
Nadia Lysenko <lysenkonk@gmail.com>
2018-09-05 10:55:28 +0300

    Applying bootstrap Styles to the Layout

--- Store/Infrastructure/PageLinkTagHelper.cs
+++ Store/Infrastructure/PageLinkTagHelper.cs
@@ -24,8 +24,15 @@ namespace Store.Infrastructure
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
+
         public PagingInfo  PageModel { get; set; }
+
         public string PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
 
         public override void Process(TagHelperContext context,
             TagHelperOutput output)
@@ -37,6 +44,13 @@ namespace Store.Infrastructure
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
                     new { productPage = i });
+                if(PageClassesEnabled)
+                {
+                   tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
