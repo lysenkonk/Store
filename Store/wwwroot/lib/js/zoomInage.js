ab03d4f17f6b436d550c8a75245ae3678712d143
lysenkonk <lysenkonk@gmail.com>
2018-11-13 10:05:02 +0200

    Change the  image view

---
+++ Store/wwwroot/lib/js/zoomInage.js
@@ -0,0 +1,34 @@
+﻿$('#productImage')
+    .addClass('zoom-in')
+    .click(function (event) {
+        event = event ? event : window.event;
+        zoomImage(event, '');
+    });
+
+function zoomImage(clk_event, zoom_url) {
+    if (zoom_url) { 
+        $('.zoomwrap')
+            .empty()
+            .append(
+                $('<img>')
+                    .attr({
+                        'src': zoom_url
+                    })
+                    .click(function () {
+                        //$('.visual.video').hide();
+                        $('.visual.image, .quality-mark').show();
+                    })
+            );
+
+        $('.visual.image, .quality-mark').hide();
+        $('.visual.video, .zoomwrap').show();
+
+        var newImagePos = _getZoomPos(clk_event, $('.zoomwrap'));
+        $('.zoomwrap img').css({
+            'top': (- newImagePos.posY) + 'px',
+            'left': (- newImagePos.posX) + 'px'
+        });
+    }
+
+    return false;
+}
