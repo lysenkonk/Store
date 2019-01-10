20fa8f0d4ad218a95ba3a8433dcddc7009cf5fdb
Nadiia <lysenkonk@gmail.com>
2019-01-10 16:21:15 +0200

    add autocoplete to add or to change categories

---
+++ Store/wwwroot/lib/js/autocoplete.js
@@ -0,0 +1,16 @@
+﻿//$(function () {
+//    $("#category").autocomplete({
+//        source: function (request, response) {
+//            $.ajax({
+//                url: "@Url.Action('GetCategories', Admin)",
+//                dataType: "json",
+//                data: { search: $("#category").val() },
+//                success: function (data) {
+//                    response($.map(data, function (item) {
+//                    }))
+//                },
+//                error: function()
+//            })
+//        }
+//    })
+//})
